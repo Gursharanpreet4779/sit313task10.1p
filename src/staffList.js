33042b6f266f4538aee80e2d0893aa1277905976
Gursharanpreet4779 <Gursharanpreetsinghsidhu38377@gmail.com>
2024-11-20 15:32:52 +0530

    Add files via upload

---
+++ src/staffList.js
@@ -0,0 +1,33 @@
+import img1 from './1.jpeg';
+import img2 from './2.webp';
+import img3 from './6.webp';
+import { faker } from '@faker-js/faker'
+const staffList =[{
+    key:0,
+    avatar : img1,
+    name : 'Melbourne',
+    description : 'It hosts major annual sporting events',
+    star : '🌟5',
+    Author : 'Emily copper'
+},
+{
+    key:1,
+    avatar : img2,
+    name : 'Morocco',
+    description : "known all over the world for its thinkers and artists",
+    star : '🌟5',
+    Author : 'halle copper'
+},
+{
+    key:2,
+    avatar : img3,
+    name : 'Paris',
+    description : "Paris is known for its gorgeous, imposing monuments and shopping",
+    star : '🌟4',
+    Author : 'James Bond'
+},
+
+
+
+]
+export default staffList
---
+++ src/tutList.js
@@ -0,0 +1,31 @@
+import img4 from './3.jpg'
+import img5 from './4.avif'
+import img6 from './5.webp'
+
+const tutList = [{
+    key:0,
+    avatar : img4,
+    name : 'London',
+    description : 'London exerts a strong influence on world art',
+    star : '🌟4',
+    Author : 'Prince William'
+},
+{
+    key:1,
+    avatar : img5,
+    name : 'Russia',
+    description : "Known all over the world for its thinkers and artists",
+    star : '🌟5',
+    Author : 'Prince Andrei'
+},
+{
+    key:2,
+    avatar : img6,
+    name : 'Italy',
+    description : "Famous for contributing to fashion",
+    star : '🌟4',
+    Author : 'Vittorio Emanuele'
+},
+]
+
+export default tutList
